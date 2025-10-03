73e40e0cf8fae2e2087c3559ef128d4d0a719c06
Brian Chen <156486616+pohaoc29@users.noreply.github.com>
2025-10-03 12:17:00 -0700

    Add preprocess.py for cleaning web-scrapped data

---
+++ preprocess.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+
+def clean_text(text):
+    if not text:
+        return "N/A"
+    text = re.sub(r"<.*?>", "", text)
+    text = re.sub(r"[^a-zA-Z0-9\s&]", "", text)
+    return text.strip() if text else "N/A"
+
+def clean_number(val):
+    if not val:
+        return 0
+    val = re.sub(r"[^\d]", "", str(val))
+    return int(val) if val.isdigit() else 0
+
+def preprocess_data(raw_data: dict):
+    return {
+        "status" : clean_text(raw_data.get("status")),
+        "type": clean_text(raw_data.get("type")),
+        "city": clean_text(raw_data.get("city")),
+        "oil_bbl": clean_number(raw_data.get("oil_bbl")),
+        "oil_desc": clean_text(raw_data.get("oil_desc")),
+        "gas_bbl": clean_number(raw_data.get("gas_bbl")),
+        "gas_desc": clean_text(raw_data.get("gas_desc"))
+    }
